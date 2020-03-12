2e1d7a45bfc57d1ddc3a05b864d2a0b5f18fe5b8
db5n17 <db5n17@soton.ac.uk>
2020-03-12 04:11:28 +0000

    Continued work on Task 5.

--- Task 5/t05sk/sample_main.c
+++ Task 5/t05sk/sample_main.c
@@ -2,20 +2,23 @@
 
 #include "os.h"
 
+#define LINES_TO_READ 5
 
 int blink(int);
 int update_dial(int);
 int collect_delta(int);
 int check_switches(int);
 
-
 FIL File;                   /* FAT File */
 
 int position = 0;
 
+TCHAR buff[2048];
+TCHAR* temp;
+TCHAR* output[5];
 
-
-void main(void) {
+void main(void) 
+{
     os_init();
 
     os_add_task( blink,            30, 1);
@@ -24,73 +27,123 @@ void main(void) {
 
     sei();
     for(;;){}
-
 }
 
 
-int collect_delta(int state) {
+int collect_delta(int state) 
+{
 	position += os_enc_delta();
 	return state;
 }
 
 
-int check_switches(int state) {
-
-	if (get_switch_press(_BV(SWN))) {
-			display_string("North\n");
+int check_switches(int state) 
+{	
+	if (get_switch_press(_BV(SWN))) 
+	{
+		display_string("North\n");
 	}
 
-	if (get_switch_press(_BV(SWE))) {
-			display_string("East\n");
+	if (get_switch_press(_BV(SWE))) 
+	{
+		display_string("East\n");
 	}
 
-	if (get_switch_press(_BV(SWS))) {
-			display_string("South\n");
+	if (get_switch_press(_BV(SWS)))
+	{
+		display_string("South\n");
 	}
 
-	if (get_switch_press(_BV(SWW))) {
+	if (get_switch_press(_BV(SWW))) 
+	{
 			display_string("West\n");
 	}
 
-	if (get_switch_long(_BV(SWC))) {
+	if (get_switch_long(_BV(SWC))) 
+	{
 		f_mount(&FatFs, "", 0);
-		if (f_open(&File, "myfile.txt", FA_WRITE | FA_OPEN_ALWAYS) == FR_OK) {
+
+		if (f_open(&File, "myfile.txt", FA_WRITE | FA_OPEN_ALWAYS) == FR_OK) 
+		{
 			f_lseek(&File, f_size(&File));
 			f_printf(&File, "Encoder position is: %d \r\n", position);
 			f_close(&File);
 			display_string("Wrote position\n");
-		} else {
-			display_string("Can't write file! \n");
+		} 
+		else 
+		{
+			display_string("Can't write file!\n");
 		}
-
 	}
 
-	if (get_switch_short(_BV(SWC))) {
-			display_string("[S] Centre\n");
+	if (get_switch_short(_BV(SWC))) 
+	{
+		display_string("[S] Centre\n");
 	}
 
-	if (get_switch_rpt(_BV(SWN))) {
-			display_string("[R] North\n");
+	if (get_switch_rpt(_BV(SWN))) 
+	{
+		display_string("[R] North\n");
 	}
 
-	if (get_switch_rpt(_BV(SWE))) {
-			display_string("[R] East\n");
+	if (get_switch_rpt(_BV(SWE))) 
+	{
+		display_string("[R] East\n");
 	}
 
-	if (get_switch_rpt(_BV(SWS))) {
-			display_string("[R] South\n");
+	if (get_switch_rpt(_BV(SWS))) 
+	{
+		display_string("[R] South\n");
+
+		f_mount(&FatFs, "", 0);
+
+		if (f_open(&File, "myfile.txt", FA_READ | FA_OPEN_ALWAYS) == FR_OK) 
+		{
+			while (f_eof(&File) == 0)
+			{
+				temp = f_gets(buff, sizeof(buff), &File);
+				display_string(temp);
+			}
+			
+			// int line = 0;
+			
+			// while (f_eof(&File) == 0)
+			// {
+			// 	if (line == LINES_TO_READ)
+			// 	{
+			// 		line = 0;
+			// 	}
+			// 	else
+			// 	{
+			// 		line++;
+			// 	}
+				
+			// 	temp = f_gets(buff, sizeof(buff), &File);
+			// 	output[line] = (TCHAR*)malloc(strlen(temp) + 1);
+			// 	strcpy(output[line], temp);
+			// }
+
+			// f_close(&File);
+
+			// for (int i = 0; i < LINES_TO_READ; i++)
+			// {
+			// 	display_string(output[i]);
+			// 	free(output[i]);
+			// }
+		} 
+		else 
+		{
+			display_string("Can't read file!\n");
+		}
 	}
 
-	if (get_switch_rpt(_BV(SWW))) {
-			display_string("[R] West\n");
+	if (get_switch_rpt(_BV(SWW))) 
+	{
+		display_string("[R] West\n");
 	}
 
-	if (get_switch_rpt(SWN)) {
-			display_string("[R] North\n");
-	}
-
-
-	if (get_switch_long(_BV(OS_CD))) {
+	if (get_switch_long(_BV(OS_CD))) 
+	{
 		display_string("Detected SD card.\n");
 	}
 
@@ -100,13 +153,17 @@ int check_switches(int state) {
 
 
 
-int blink(int state) {
+int blink(int state) 
+{
 	static int light = 0;
 	uint8_t level;
 
-	if (light < -120) {
+	if (light < -120) 
+	{
 		state = 1;
-	} else if (light > 254) {
+	} 
+	else if (light > 254)
+	{
 		state = -20;
 	}
 
@@ -114,28 +171,40 @@ int blink(int state) {
 	/* Compensate somewhat for nonlinear LED
            output and eye sensitivity:
         */
-	if (state > 0) {
-		if (light > 40) {
+	if (state > 0) 
+	{
+		if (light > 40) 
+		{
 			state = 2;
 		}
-		if (light > 100) {
+		if (light > 100)
+		{
 			state = 5;
 		}
-	} else {
-		if (light < 180) {
+	} 
+	else 
+	{
+		if (light < 180) 
+		{
 			state = -10;
 		}
-		if (light < 30) {
+		if (light < 30) 
+		{
 			state = -5;
 		}
 	}
 	light += state;
 
-	if (light < 0) {
+	if (light < 0) 
+	{
 		level = 0;
-	} else if (light > 255) {
+	} 
+	else if (light > 255) 
+	{
 		level = 255;
-	} else {
+	} 
+	else 
+	{
 		level = light;
 	}
 
